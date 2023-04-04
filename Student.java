cab10984bf4a341ae34ad82db244c7c6d0b1e1d4
soundari4 <128634283+soundari4@users.noreply.github.com>
2023-04-04 19:44:21 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,34 @@
+package sample;
+
+public class Student {
+	int id;
+	String firstName;
+	String secondName;
+	int age;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getSecondName() {
+		return secondName;
+	}
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+
+}
